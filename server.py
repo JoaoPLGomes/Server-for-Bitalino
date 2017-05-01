7154920b3a5051ed1b09d5b3a1c841de42c43a8d
JoaoPLGomes <joaogomesapps@gmail.com>
2017-05-01 16:04:41 +0100

    Supports multiple connections
    Created a new thread that listen to Bitalino, so that the main thread is
    not occupied.

--- server.py
+++ server.py
@@ -4,14 +4,16 @@ import tornado.ioloop
 import tornado.web
 import socket, test
 from tornado import gen
+import threading
 '''
 This is a simple Websocket Echo server that uses the Tornado websocket handler.
 Please run `pip install tornado` with python of version 2.7.9 or greater to install tornado.
 This program will echo back the reverse of whatever it recieves.
 Messages are output to the terminal for debuggin purposes. 
 ''' 
+threads = [] #Array with threads
+connections = set() #Clients that are connected
 
-connections = set()
 class WSHandler(tornado.websocket.WebSocketHandler):
 	
 	
@@ -22,10 +24,8 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 	def open(self):
 		connections.add(self)
 		print 'New connection was opened'
-		if len(connections) == 1:
-			read_function()
-		else :
-			self.write_message("Conn!")
+		
+		self.write_message("Conn!")
 
 
 	
@@ -52,21 +52,27 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 
 def read_function():
     while True:
-        data = test.info_loop()
-        print data
+
+    	if len(connections) >= 1:
+			
+        	data = test.info_loop()
+        	print data
         
-        [client.write_message(data) for client in connections]
+        	[client.write_message(data) for client in connections]
 
 
 application = tornado.web.Application([
 	(r'/ws', WSHandler),
 ])
  
- 
+
 if __name__ == "__main__":
 	http_server = tornado.httpserver.HTTPServer(application)
 	http_server.listen(65)
-	
+	t = threading.Thread(target=read_function)
+	threads.append(t)
+	t.start()
 	tornado.ioloop.IOLoop.instance().start()
+
 
 #[con.write_message('Hi!') for con in self.connections] -- Para enviar mensagens para todos os clientes !
